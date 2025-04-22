051d5a607e447ac44fb1eecf7e9ccbd888554f05
Gabriel Melo <gabrimelo070@gmail.com>
2025-04-22 14:25:06 -0300

    feat: create Ninja entity

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/NinjaModel.java
@@ -0,0 +1,49 @@
+package dev.java10x.CadastroDeNinjas;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_ninja")
+public class NinjaModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String name;
+    private String email;
+    private int age;
+
+    public NinjaModel() {
+    }
+
+    public NinjaModel(String name, String email, int age) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
